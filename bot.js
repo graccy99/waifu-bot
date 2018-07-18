0b6cca5d6ec8c512089b3ec2e45cee490bfa9b2f
john <graccy99@gmail.com>
2018-07-17 21:53:06 -0400

    sdf

--- bot.js
+++ bot.js
@@ -34,47 +34,47 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         to: channelID,
                         message: 'https://pre00.deviantart.net/7f26/th/pre/f/2017/162/a/f/megumin__exploooosionnn_by_squchan-dbcbquw.png'
                     });
-                } else if (x == 1) {
+                } else if (x == 2) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'http://goboiano.com/wp-content/uploads/2017/06/Re-Zero-10.png'
                     });
-                } else if (x == 2) {
+                } else if (x == 3) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://cdn.suwalls.com/wallpapers/anime/taiga-aisaka-toradora-33412-400x250.jpg'
                     });
-                } else if (x == 3) {
+                } else if (x == 4) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'http://pm1.narvii.com/6810/51f15caaa31e53acd91efa1f25a5aae1b8683ad2v2_00.jpg'
                     });
-                } else if (x == 4) {
+                } else if (x == 5) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://static.zerochan.net/Matsumae.Ohana.full.449038.jpg'
                     });
-                } else if (x == 5) {
+                } else if (x == 6) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://i.imgur.com/mQegmiL.jpg'
                     });
-                } else if (x == 6) {
+                } else if (x == 7) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://vignette.wikia.nocookie.net/maid-dragon/images/5/57/Kanna_Anime.png/revision/latest?cb=20180225164809'
                     });
-                } else if (x == 7) {
+                } else if (x == 8) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://pbs.twimg.com/media/DI9SAbaW4AAiKFC.jpg'
                     });
-                } else if (x == 8) {
+                } else if (x == 9) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://78.media.tumblr.com/1022be09cfb5ebd7d10d817865fb5d8f/tumblr_oqvxcctpzb1vze779o2_400.png'
                     });
-                } else if (x == 9) {
+                } else if (x == 10) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://i.imgur.com/3zvzQpZ.png'
